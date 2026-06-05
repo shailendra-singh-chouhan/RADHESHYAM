0df1d497f3bb94284356f2780aad5fe5a87de84f
shailendra-singh-chouhan <ajmer.shailendra@gmail.com>
2026-06-05 21:06:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, jsonify
+import yfinance as yf
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Ye tumhari dashboard.html file ko serve karega
     return send_file('dashboard.html')
 
+@app.route('/api/data')
+def get_market():
+    try:
+        ticker = yf.Ticker("^NSEI")
+        data = ticker.history(period="1d")
+        price = data['Close'].iloc[-1]
+        return jsonify({"price": round(price, 2)})
+    except:
+        return jsonify({"price": 24408.5})
+
 if __name__ == '__main__':
-    # Render ke liye zaroori configuration
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
